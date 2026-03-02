5765e0730446c26d2641b8f21b4d476b0a03f874
Larry Gouger <larry.gouger@coxautoinc.com>
2026-03-02 09:23:20 -0500

    added a bit of ai

--- init.el
+++ init.el
@@ -1,4 +1,4 @@
-;;; init.el -- setup my emacs environment
+;;;pac.el -- setup my emacs environment
 
 ;;; Commentary:
 
@@ -20,96 +20,97 @@
 
 (require 'package)
 
-(when (< emacs-major-version 27)
-  (package-initialize))
+;; (when (< emacs-major-version 27)
+;;   (package-initialize))
 
 (defvar predicate nil)
 (defvar inherit-input-method nil)
+
 
 
 ;; The packages you want installed. You can also install these
 ;; manually with M-x package-install
 ;; Add in your own as you wish:
 (defvar my-packages
-      '(use-package
+  '(use-package
 
-        org
+     org
 
-        ;; used by use-package
-        diminish
-        delight
+     ;; used by use-package
+     diminish
+     delight
 
-	;; project navigation
-        projectile
+     ;; project navigation
+     projectile
 
-        ;; Modular in-buffer completion framework for Emacs. http://company-mode.github.io
-        company
+     ;; Modular in-buffer completion framework for Emacs. http://company-mode.github.io
+     company
 
-        ;; completion:  tried ivy, ido, and helm
-        vertico
+     ;; completion:  tried ivy, ido, and helm
+     vertico
 
-        ;; trying out Language Server Protocol mode
-        ;; lsp-mode
-        ;; lsp-python-ms
+     ;; testing related
+     buttercup
 
-        ;; linting of languages
-        flycheck
+     ;; linting of languages
+     flycheck
+     
+     ;; makes handling lisp expressions much, much easier
+     ;; Cheatsheet: http://www.emacswiki.org/emacs/PareditCheatsheet
+     paredit
 
-	;; makes handling lisp expressions much, much easier
-	;; Cheatsheet: http://www.emacswiki.org/emacs/PareditCheatsheet
-        paredit
+     ;; colorful parenthesis matching
+     rainbow-delimiters
 
-	;; colorful parenthesis matching
-        rainbow-delimiters
+     ;; key bindings and code colorization for Clojure
+     ;; https://github.com/clojure-emacs/clojure-mode
+     clojure-mode
 
-	;; key bindings and code colorization for Clojure
-	;; https://github.com/clojure-emacs/clojure-mode
-        clojure-mode
+     ;; integration with a Clojure REPL
+     ;; https://github.com/clojure-emacs/cider
+     cider
 
-	;; integration with a Clojure REPL
-	;; https://github.com/clojure-emacs/cider
-        cider
+     ;; Python mode
+     elpy
+     py-autopep8
+     ;; pipenv
 
-	;; Python mode
-        elpy
-        py-autopep8
-        ;; pipenv
+     ;; AI stuff
+     eca
 
-	;; Enhances M-x to allow easier execution of commands. Provides
-	;; a filterable list of possible commands in the minibuffer
-	;; http://www.emacswiki.org/emacs/Smex
-        ;; smex
+     ;; edit html tags like sexps
+     ;; tagedit
 
-	;; edit html tags like sexps
-        ;; tagedit
+     ;; git integration
+     magit
 
-	;; git integration
-        magit
+     ;; json editing
+     json-mode
 
-	;; json editing
-        json-mode
+     ;; yaml editing
+     yaml-mode
 
-	;; yaml editing
-        yaml-mode
+     ;; kotlin editing
+     kotlin-mode
 
-	;; kotlin editing
-        kotlin-mode
+     ;; groovy editing
+     groovy-mode
 
-        ;; groovy editing
-        groovy-mode
+     ;; Terraform and HCL
+     hcl-mode
+     terraform-mode
 
-	;; Terraform and HCL
-        hcl-mode
-        terraform-mode
+     ;; ai agent-shell mode stuff
+     ;; agent-shell
 
-        ;; REST client
-        restclient
-        ob-restclient
+     ;; REST client
+     restclient
+     ob-restclient
 
-	;; misc
-        which-key
-        spaceline
-        ))
+     ;; misc
+     which-key
+     spaceline
+     ))
 
 ;; On OS X, an Emacs instance started from the graphical user
 ;; interface will have a different environment than a shell in a
@@ -120,9 +121,11 @@
 ;; environment variables from the user's shell.
 ;; https://github.com/purcell/exec-path-from-shell
 
-(if (eq system-type 'darwin)
-    (add-to-list 'my-packages 'exec-path-from-shell))
+;; (if (eq system-type 'darwin)
+;;     (add-to-list 'my-packages 'exec-path-from-shell))
 
+(when (memq window-system '(mac ns x))
+  (exec-path-from-shell-initialize))
 
 (defun my-packages-installed-p ()
 
@@ -191,6 +194,8 @@
 ;; setup for varios programming languages
 (load "development.el")
 
+(load "ai.el")
+
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -199,100 +204,33 @@
  ;; If there is more than one, they won't work right.
  '(coffee-tab-width 2)
  '(connection-local-criteria-alist
-   '(((:application eshell) eshell-connection-default-profile)
+   '(((:application tramp :protocol "kubernetes")
+      tramp-kubernetes-connection-local-default-profile)
+     ((:application eshell) eshell-connection-default-profile)
      ((:application tramp :machine "localhost")
-      tramp-connection-local-darwin-ps-profile)
-     ((:application tramp :machine "LBURmac3099LVDT")
       tramp-connection-local-darwin-ps-profile)
      ((:application tramp)
       tramp-connection-local-default-system-profile
       tramp-connection-local-default-shell-profile)))
- '(connection-local-profile-alist
-   '((eshell-connection-default-profile (eshell-path-env-list))
-     (tramp-connection-local-darwin-ps-profile
-      (tramp-process-attributes-ps-args "-acxww" "-o"
-                                        "pid,uid,user,gid,comm=abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-                                        "-o" "state=abcde" "-o"
-                                        "ppid,pgid,sess,tty,tpgid,minflt,majflt,time,pri,nice,vsz,rss,etime,pcpu,pmem,args")
-      (tramp-process-attributes-ps-format (pid . number)
-                                          (euid . number)
-                                          (user . string)
-                                          (egid . number) (comm . 52)
-                                          (state . 5) (ppid . number)
-                                          (pgrp . number)
-                                          (sess . number)
-                                          (ttname . string)
-                                          (tpgid . number)
-                                          (minflt . number)
-                                          (majflt . number)
-                                          (time . tramp-ps-time)
-                                          (pri . number)
-                                          (nice . number)
-                                          (vsize . number)
-                                          (rss . number)
-                                          (etime . tramp-ps-time)
-                                          (pcpu . number)
-                                          (pmem . number) (args)))
-     (tramp-connection-local-busybox-ps-profile
-      (tramp-process-attributes-ps-args "-o"
-                                        "pid,user,group,comm=abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-                                        "-o" "stat=abcde" "-o"
-                                        "ppid,pgid,tty,time,nice,etime,args")
-      (tramp-process-attributes-ps-format (pid . number)
-                                          (user . string)
-                                          (group . string) (comm . 52)
-                                          (state . 5) (ppid . number)
-                                          (pgrp . number)
-                                          (ttname . string)
-                                          (time . tramp-ps-time)
-                                          (nice . number)
-                                          (etime . tramp-ps-time)
-                                          (args)))
-     (tramp-connection-local-bsd-ps-profile
-      (tramp-process-attributes-ps-args "-acxww" "-o"
-                                        "pid,euid,user,egid,egroup,comm=abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-                                        "-o"
-                                        "state,ppid,pgid,sid,tty,tpgid,minflt,majflt,time,pri,nice,vsz,rss,etimes,pcpu,pmem,args")
-      (tramp-process-attributes-ps-format (pid . number)
-                                          (euid . number)
-                                          (user . string)
-                                          (egid . number)
-                                          (group . string) (comm . 52)
-                                          (state . string)
-                                          (ppid . number)
-                                          (pgrp . number)
-                                          (sess . number)
-                                          (ttname . string)
-                                          (tpgid . number)
-                                          (minflt . number)
-                                          (majflt . number)
-                                          (time . tramp-ps-time)
-                                          (pri . number)
-                                          (nice . number)
-                                          (vsize . number)
-                                          (rss . number)
-                                          (etime . number)
-                                          (pcpu . number)
-                                          (pmem . number) (args)))
-     (tramp-connection-local-default-shell-profile
-      (shell-file-name . "/bin/sh") (shell-command-switch . "-c"))
-     (tramp-connection-local-default-system-profile
-      (path-separator . ":") (null-device . "/dev/null"))))
- '(org-safe-remote-resources '("\\`https://fniessen\\.github\\.io\\(?:/\\|\\'\\)"))
+  '(org-agenda-files
+   '("~/org/notes.org"
+     "/Users/larry.gouger/org/work.org"
+     "/Users/larry.gouger/org/home.org"))
  '(package-selected-packages
-   '(all-the-icons amx base16-theme clj-refactor clojure-ts-mode
-                   company-box dashboard delight diff-hl diminish elpy
-                   embark embrace exec-path-from-shell flx-ido
-                   flycheck groovy-mode highlight-indent-guides
-                   json-mode kotlin-mode lorem-ipsum marginalia
-                   nerd-icons-completion ob-async ob-restclient
-                   orderless org-bullets ox-gfm ox-reveal pipenv
-                   poetry py-autopep8 python-black rainbow-delimiters
-                   rust-mode spaceline spaceline-config
-                   string-inflection tagedit terraform-mode
-                   treemacs-icons-dired treemacs-magit
-                   treemacs-projectile treesit-auto vertico which-key
-                   winum yaml-mode)))
+   '(agent-shell all-the-icons amx base16-theme buttercup clj-refactor
+                 clojure-ts-mode company-box dashboard delight diff-hl
+                 diminish eca elpy embark embrace exec-path-from-shell
+                 flx-ido flycheck groovy-mode highlight-indent-guides
+                 json-mode kotlin-mode lorem-ipsum marginalia
+                 nerd-icons-completion ob-async ob-restclient
+                 orderless org-bullets ox-gfm ox-reveal pipenv poetry
+                 py-autopep8 python-black rainbow-delimiters rust-mode
+                 spaceline string-inflection tagedit terraform-mode
+                 treemacs-icons-dired treemacs-magit
+                 treemacs-projectile treesit-auto vertico which-key
+                 winum yaml-mode))
+ '(package-vc-selected-packages
+   '((eca :url "http://github.com/editor-code-assistant/eca-emacs"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
